c47d4501fca4a40514121e0012425e5ffb5aeae0
김은결 <kyeolrii@gim-eungyeol-ui-MacBookPro.local>
2025-10-26 16:53:29 +0900

    second commit

--- Downloads/number 2/src/data/projects.ts
+++ Downloads/number 2/src/data/projects.ts
@@ -1,10 +1,20 @@
 import { Project } from "@/types/project";
 
 export const projects = [
-  { id: 1, title: "About Beauty", year: 2024, position: { top: 20, left: 70 } },
-  { id: 2, title: "700-08", year: 2024, position: { top: 25, left: 60 } },
-  { id: 3, title: "To My Ideal", year: 2022, position: { top: 35, left: 45 } },
-  { id: 4, title: "Seoul Distortion", year: 2022, position: { top: 15, left: 30 } },
-  { id: 5, title: "A Choir of Monologues", year: 2024, position: { top: 10, left: 32 } },
+  { id: 1, number: "1", title: "About Beauty", year: 2024, position: { top: 20, left: 70 } },
+  { id: 2, number: "2", title: "700-08", year: 2024, position: { top: 25, left: 60 } },
+  { id: 3, number: "3", title: "To My Ideal", year: 2022, position: { top: 35, left: 45 } },
+  { id: 4, number: "4", title: "Seoul Distortion", year: 2022, position: { top: 15, left: 30 } },
+  { id: 5, number: "5", title: "A Choir of Monologues", year: 2024, position: { top: 10, left: 32 } },
+  { id: 6, number: "6", title: "Typemoji", year: 2025, position: { top: 10, left: 32 } },
+  { id: 7, number: "7", title: "Prova", year: 2025, position: { top: 10, left: 32 } },
+  { id: 8, number: "8", title: "Overlock", year: 2024, position: { top: 10, left: 32 } },
+  { id: 9, number: "9", title: "Long-Talk Film Festival", year: 2024, position: { top: 10, left: 32 } },
+  { id: 10, number: "10", title: "Unsocialized Residue", year: 2023, position: { top: 10, left: 32 } },
+  { id: 11, number: "11", title: "We are Aliens", year: 2023, position: { top: 10, left: 32 } },
+  { id: 11, number: "12", title: "We are Aliens", year: 2023, position: { top: 10, left: 32 } },
+
+
+
   // … 계속
 ];
